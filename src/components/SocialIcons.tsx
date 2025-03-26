343df0e9812e0326d05b22247569bd3367e1a827
shouvik18 <shouvikpaulofficial@gmail.com>
2025-03-26 08:45:16 +0530

    Update SocialIcons.tsx

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -2,9 +2,9 @@ import { Github, Instagram, Linkedin } from 'lucide-react';
 
 function SocialIcons() {
   const socialLinks = [
-    { name: 'Github', icon: <Github />, link: 'https://www.github.com/lohitkolluri' },
-    { name: 'LinkedIn', icon: <Linkedin />, link: 'https://www.linkedin.com/in/kollurilohit/' },
-    { name: 'Instagram', icon: <Instagram />, link: 'https://instagram.com/kollurilohit' },
+    { name: 'Github', icon: <Github />, link: 'https://www.github.com/shouvik18' },
+    { name: 'LinkedIn', icon: <Linkedin />, link: 'https://www.linkedin.com/in/shouvikpal/' },
+    { name: 'Instagram', icon: <Instagram />, link: 'https://www.instagram.com/shouvik.pal_' },
   ];
 
   return (
